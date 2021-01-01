7242daa9db7c6fd0307be3137db844dbd9b5b4ae
ming19956 <ming19956@hotmail.com>
2021-01-01 18:59:42 +0100

    add the pre-trained bert model download link

--- information-retrival-search-engine/informationRetrival/manage.py
+++ information-retrival-search-engine/informationRetrival/manage.py
@@ -7,5 +7,8 @@ if __name__ == "__main__":
 
     from django.core.management import execute_from_command_line
 
-    os.system("bert-serving-start -model_dir /Users/yma/Downloads/wwm_uncased_L-24_H-1024_A-16 -num_worker=2")
+    os.system("bert-serving-start -model_dir /Users/yma/Downloads/wwm_uncased_L-12_H-768_A-12 -num_worker=2")
     execute_from_command_line(sys.argv)
+
+
+## Download the document with the link : storage/googleapis.com/bert_models/2018_10_18/uncased_L-12_H-768_A-12.zip
